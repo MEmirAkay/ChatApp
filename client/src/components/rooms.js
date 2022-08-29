533e65e92848033887a6dcfb0c2f5cb50985c781
Emir Akay <emirakay073@gmail.com>
2022-08-29 15:06:24 +0300

    commit

--- client/src/components/rooms.js
+++ client/src/components/rooms.js
@@ -3,12 +3,14 @@ export default class Rooms extends Component {
   render() {
     const rooms = [1, 2, 3, 4, 5]; //rooms
     return (
-      <div className="flex flex-col">
+      <div className="flex flex-col w-full px-5 pt-2">
         {rooms.map((e) => {
           return (
             <button
-              className="focus:bg-gray-800 focus:shadow-gray-800/20 hover:shadow-lg hover:shadow-gray-800/20  text-white py-4 m-1 hover:bg-gray-800 rounded-md duration-200 outline-none"
+            key={e}
+              className="focus:bg-gray-700 focus:shadow-gray-700/20 hover:shadow-lg w-full hover:shadow-gray-800/20  text-white py-5 m-1 hover:bg-gray-800 rounded-md duration-200 outline-none bg-gray-800"
               onClick={() => {
+                
                 this.props.setRoom(e);
                 this.props.chatUpdate(e);
               }}
